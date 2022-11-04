32a26d4f4eff33c404cfe992cdc677c144c1c08e
Biki <102693150+Bikash1075@users.noreply.github.com>
2022-11-05 00:57:13 +0530

    Add files via upload

--- pr2.py
+++ pr2.py
@@ -1,10 +1,3 @@
-# Write a Python program to triple 
-# all numbers of a given list of integers. 
-# Use Python map.
-# sample list: [1, 2, 3, 4, 5, 6, 7]
-# Triple of list numbers:
-# [3, 6, 9, 12, 15, 18, 21]
-# Must use map to find the expected output - 30
 l=[1, 2, 3, 4, 5, 6, 7]
 l1=map(lambda x:x*3,l)
 print(list(l1))
