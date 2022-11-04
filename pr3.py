a8b86f30c162a2db48d73606ef3a822ab74579ac
Biki <102693150+Bikash1075@users.noreply.github.com>
2022-11-05 00:57:42 +0530

    Add files via upload

--- pr3.py
+++ pr3.py
@@ -1,12 +1,3 @@
-# Write a Python program to square the 
-# elements of a list using map() function.
-# Sample List: [4, 5, 2, 9]
-# Square the elements of the list:
-# [16, 25, 4, 81]
-# Must have list as the input - 15
-# Must Use map() function to solve the problem - 15
-# Must give the expected output - 10
-
 l=(map(int,input("Enter numbers ").split()))
 l=(map(lambda x:x*x,l))
 print(list(l))
