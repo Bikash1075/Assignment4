292b783a6866f2f8515ec2b027cdf8ebe23f1a44
Biki <102693150+Bikash1075@users.noreply.github.com>
2022-11-05 00:51:00 +0530

    Add files via upload

---
+++ pr1.py
@@ -0,0 +1,11 @@
+# # Write a Python program to create 
+# a lambda function that adds 
+# 25 to a given number passed in as an argument.
+# sample input: 10
+# sample output: 35
+# should use lambda function to 
+# solve the problem - 15
+# should print the output - 15
+x=10
+y=lambda x:x+25
+print(y(x))
